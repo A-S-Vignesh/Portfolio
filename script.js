ccb74dbb7e6346eb0089cea2ae863c15fdaf3f49
Halo-One <asvicki2002@gmail.com>
2024-05-24 16:29:02 +0530

    update scroll bar

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ $(document).ready(function()
 {
   // navigation bar active link start
   var navigationItem = document.querySelectorAll(".navlink");
-  var preNavItem = navigationItem[0];
   for (var i = 0; i < navigationItem.length; i++) {
     navigationItem[i].addEventListener("click", function (event) {
-      preNavItem.classList.remove("active");
-      preNavItem = event.target;
-      event.target.classList.add("active");
+      navigationItem.forEach(function (link) {
+        link.classList.remove("active");
+      });
+      navigationItem[i].classList.add("active");
     });
   }
 
   //   Navigation bar active link ends
-  var lastScrollTop = 0;
+//   var lastScrollTop = 0;
 
-  $(window).scroll(function (event) {
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop) {
-      // Scroll down
-      $("#navbar").addClass("navbar-hidden");
-    } else {
-      // Scroll up
-      $("#navbar").removeClass("navbar-hidden");
-    }
-    lastScrollTop = st;
-  });
+//   $(window).scroll(function (event) {
+//     var st = $(this).scrollTop();
+//     if (st > lastScrollTop) {
+//       // Scroll down
+//       $("#navbar").addClass("navbar-hidden");
+//     } else {
+//       // Scroll up
+//       $("#navbar").removeClass("navbar-hidden");
+//     }
+//     lastScrollTop = st;
+//   });
 
   var allSection = document.querySelectorAll("section"); //selecting all section
   window.addEventListener("scroll", function () {
     var scrollPosition = document.documentElement.scrollTop;
 
     for (let i = 0; i < navigationItem.length; i++) {
-      var sectionTop = allSection[i].offsetTop +5;
+      var sectionTop = allSection[i].offsetTop-80;
       var sectionBottom = sectionTop + allSection[i].offsetHeight;
 
       if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
